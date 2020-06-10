4b4c5cefa0e661c684cd655ca5494570a10d9e38
Bekzod Allaev <Allaev_@outlook.com>
2020-06-10 12:18:09 +0500

    Little changes + connection string was added

--- AllaevOMSWebMini/Data/NorthwindContext.cs
+++ AllaevOMSWebMini/Data/NorthwindContext.cs
@@ -25,6 +25,15 @@ namespace AllaevOMSWebMini.Data
         public virtual DbSet<Shipper> Shippers { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
 
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=ALLAEV\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+//            }
+//        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>(entity =>
