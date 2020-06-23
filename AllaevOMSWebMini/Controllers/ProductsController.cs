eb768de7953d8971acb4e443084896dd521896a3
Bekzod Allaev <Allaev_@outlook.com>
2020-06-23 16:28:56 +0500

    GetProducts method is ready

--- AllaevOMSWebMini/Controllers/ProductsController.cs
+++ AllaevOMSWebMini/Controllers/ProductsController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AllaevOMSWebMini.Data;
+using AllaevOMSWebMini.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AllaevOMSWebMini.Controllers
 {
@@ -11,5 +14,19 @@ namespace AllaevOMSWebMini.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private NorthwindContext northwindContext;
+
+        public ProductsController(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+        }
+
+        // api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await northwindContext.Products.ToListAsync();
+        }
+
     }
 }
