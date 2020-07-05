73b26879a4c902006de59799caacaf409d7b99f8
Bekzod Allaev <Allaev_@outlook.com>
2020-07-05 23:10:07 +0500

    Get endpoints are ready

--- OMSWebMini/Controllers/EmployesController.cs
+++ OMSWebMini/Controllers/EmployesController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OMSWebMini.Data;
+using OMSWebMini.Model;
 
 namespace OMSWebMini.Controllers
 {
@@ -11,7 +14,50 @@ namespace OMSWebMini.Controllers
     [ApiController]
     public class EmployesController : ControllerBase
     {
+        NorthwindContext northwindContext;
 
+        public EmployesController(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+        }
 
+        //GET api/Employes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAll(bool showReports = false, bool showPhoto = false)
+        {
+            var employes = await northwindContext.Employees.ToListAsync();
+
+            if (showPhoto & showReports)
+                return employes;
+            else if (!showPhoto & !showReports)
+                await Task.Run(() =>
+                {
+                    employes.ForEach(a =>
+                    {
+                        a.Photo = null;
+                        a.ReportsToNavigation = null;
+                        a.InverseReportsToNavigation = null;
+                    });
+                });
+            else if (!showReports)
+                await Task.Run(() => { employes.ForEach(a => a.ReportsToNavigation = null); });
+            else
+                await Task.Run(() => { employes.ForEach(a => a.Photo = null); });
+
+            return employes;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetById(int id)
+        {
+            var employee = await northwindContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
     }
 }
