630fea6cb528d33ed0692788e62da38ffb1cf8b7
Bekzod Allaev <Allaev_@outlook.com>
2020-07-06 19:51:04 +0500

    Get endpoints are ready

---
+++ OMSWebMini/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OMSWebMini.Data;
+using OMSWebMini.Model;
+
+namespace OMSWebMini.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        NorthwindContext northwindContext;
+
+        public CustomersController(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await northwindContext.Customers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(string id)
+        {
+            var customer = await northwindContext.Customers.FindAsync(id);
+
+            if (customer == null) return NotFound();
+
+            return customer;
+        }
+    }
+}
