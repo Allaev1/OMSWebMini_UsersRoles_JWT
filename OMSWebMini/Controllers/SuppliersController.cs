fc7d5014fa34e6d4556e85b1d20681e4d420b434
Bekzod Allaev <Allaev_@outlook.com>
2020-07-06 21:00:39 +0500

    Supplier controller was added and Get endpoints were designed

---
+++ OMSWebMini/Controllers/SuppliersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OMSWebMini.Data;
+using OMSWebMini.Model;
+
+namespace OMSWebMini.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        NorthwindContext northwindContext;
+
+        public SuppliersController(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await northwindContext.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await northwindContext.Suppliers.FindAsync(id);
+
+            if (supplier == null) return NotFound();
+
+            return supplier;
+        }
+    }
+}
