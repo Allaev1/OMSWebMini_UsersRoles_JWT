271f1e1cb7437d8221b027fb416a7c74ca4c1d36
Bekzod Allaev <Allaev_@outlook.com>
2020-07-08 14:11:27 +0500

    Shipper controller was added and Get enpoints were added

--- OMSWebMini/Controllers/ShippersController.cs
+++ OMSWebMini/Controllers/ShippersController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OMSWebMini.Data;
+using OMSWebMini.Model;
 
 namespace OMSWebMini.Controllers
 {
@@ -11,5 +14,27 @@ namespace OMSWebMini.Controllers
     [ApiController]
     public class ShippersController : ControllerBase
     {
+        NorthwindContext northwindContext;
+
+        public ShippersController(NorthwindContext northwindContext)
+        {
+            this.northwindContext = northwindContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shipper>>> GetShippers()
+        {
+            return await northwindContext.Shippers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shipper>> GetShipper(int id)
+        {
+            var shipper = await northwindContext.Shippers.FindAsync(id);
+
+            if (shipper == null) return NotFound();
+
+            return shipper;
+        }
     }
 }
